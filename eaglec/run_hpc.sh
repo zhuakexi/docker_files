2106c32bc2c56ef63ba85c252b49e58381aaeec2
zhuakexi <zhuakexi@126.com>
2023-03-29 14:19:59 +0800

    adding eaglec env

---
+++ eaglec/run_hpc.sh
@@ -0,0 +1,1 @@
+docker run -it --name eaglec --mount type=bind,src=/share/Data/ychi,target=/share/Data/ychi --mount type=bind,src=/share/home/ychi,target=/share/home/ychi --mount type=bind,src=/shareb/ychi,target=/shareb/ychi zhuakexi/eaglec_v01
