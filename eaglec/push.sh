e705c432d0f9211115faad619f17896adb9454b3
zhuakexi <zhuakexi@126.com>
2023-04-04 22:06:16 +0800

    unzip models, and using numpy 1.21 because  failed with newer numpy

--- eaglec/push.sh
+++ eaglec/push.sh
@@ -1,4 +1,4 @@
 #docker tag zhuakexi/eaglec:0.2 zhuakexi/xtw_eaglec:v1.0
 #docker push zhuakexi/xtw_eaglec:v1.0
-docker tag zhuakexi/eaglec:0.3 zhuakexi/xtw_eaglec:v1.1
-docker push zhuakexi/xtw_eaglec:v1.1
+docker tag zhuakexi/eaglec:0.5 zhuakexi/xtw_eaglec:v1.3
+docker push zhuakexi/xtw_eaglec:v1.3
