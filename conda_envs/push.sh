5425ae8f6cf97484e30cb19d00eedbcdb00e5392
zhuakexi <zhuakexi@126.com>
2023-11-06 10:45:25 +0800

    adding snakemake env

--- conda_envs/push.sh
+++ conda_envs/push.sh
@@ -1,2 +1,2 @@
-docker tag zhuakexi/bioenvs:v0.2.4 zhuakexi/bioenvs:v0.2.4
-docker push zhuakexi/bioenvs:v0.2.4
+docker tag zhuakexi/bioenvs:v0.2.5 zhuakexi/bioenvs:v0.2.5
+docker push zhuakexi/bioenvs:v0.2.5
