9dfafb255d9bb1ea277c2771243cf5d6850c666d
zhuakexi <zhuakexi@126.com>
2023-11-29 17:14:07 +0800

    move base to micromamba

--- hic_basic/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Activate the conda base environment
-. /miniconda3/etc/profile.d/conda.sh
-conda activate base
-
-# Launch the command provided by the user
-exec "$@"
--- hic_basic/push.sh
+++ hic_basic/push.sh
@@ -1,2 +1,2 @@
-docker tag zhuakexi/hic_basic:v0.1 zhuakexi/zhuakexi:v0.1
-docker push zhuakexi/hic_basic:v0.1
+#docker tag zhuakexi/hic_basic:v0.2 zhuakexi/zhuakexi:v0.2
+docker push zhuakexi/hic_basic:v0.2
