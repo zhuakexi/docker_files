1400e1e2b39f26e5d9ddd8c206500e302c659f00
zhuakexi <zhuakexi@126.com>
2023-11-29 14:49:24 +0800

    update to v0.2.6, adding contrib version of opencv

--- conda_envs/build.sh
+++ conda_envs/build.sh
@@ -1,1 +1,1 @@
-docker build -t zhuakexi/bioenvs:v0.2.5 --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) .
+docker build -t zhuakexi/bioenvs:v0.2.6 --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) .
--- conda_envs/push.sh
+++ conda_envs/push.sh
@@ -1,2 +1,2 @@
-docker tag zhuakexi/bioenvs:v0.2.5 zhuakexi/bioenvs:v0.2.5
-docker push zhuakexi/bioenvs:v0.2.5
+docker tag zhuakexi/bioenvs:v0.2.6 zhuakexi/bioenvs:v0.2.6
+docker push zhuakexi/bioenvs:v0.2.6
