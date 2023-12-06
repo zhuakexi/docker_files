e80bc8cc1803aa58bd3aa859bb196d91ea594ed4
zhuakexi <zhuakexi@126.com>
2023-12-06 12:06:31 +0800

    also freeze pypi packages for future rebuild

--- hic_basic/lock.sh
+++ hic_basic/lock.sh
@@ -1,3 +1,7 @@
 docker run --rm -v "$(pwd):/tmp" \
    zhuakexi/hic_basic:v0.4 /bin/bash -c "\
      micromamba env export --name base --explicit" > base.lock
+
+docker run --rm -v "$(pwd):/tmp" \
+   zhuakexi/hic_basic:v0.4 /bin/bash -c "\
+     pip freeze | grep -E 'ipykernel|open3d|opencv' > requirements.txt"
