6c0619bbbd364f768514e59f768a7f17cac64257
zhuakexi <zhuakexi@126.com>
2023-12-06 11:37:38 +0800

    try to migrate to jupyter docker stack, not working yet

--- conda_envs/run_hpc.sh
+++ conda_envs/run_hpc.sh
@@ -1,3 +1,7 @@
 # jupyter 8087
 #docker run -p 8087:8087 -e JUPYTER_ENABLE_LAB=yes -e JUPYTER_TOKEN=docker --name jupyter_ychi --mount type=bind,src=/share/Data/ychi,target=/share/Data/ychi --mount type=bind,src=/share/home/ychi,target=/share/home/ychi --mount type=bind,src=/shareb/ychi,target=/shareb/ychi zhuakexi/jupyterbio:conda_s2.8.1
-docker run --rm -p 8087:8087 -e JUPYTER_ENABLE_LAB=yes -e JUPYTER_TOKEN=docker --name bioenvs_ychi --mount type=bind,src=/share/Data/ychi,target=/share/Data/ychi --mount type=bind,src=/share/home/ychi,target=/share/home/ychi --mount type=bind,src=/shareb/ychi,target=/shareb/ychi --mount type=bind,src=/sharec/ychi/,target=/sharec/ychi zhuakexi/bioenvs:v0.2.6 /bin/bash
+docker run -it --rm -p 8888:8888
+docker run -it --rm -p 8087:8087 --name bioenvs_ychi -e JUPYTER_PORT=8087 \
+--user $(id -u) --group-add users -e CHOWN_HOME=yes \
+-e RESTARTABLE=yes \
+--mount type=bind,src=/share/Data/ychi,target=/share/Data/ychi --mount type=bind,src=/share/home/ychi,target=/share/home/ychi --mount type=bind,src=/shareb/ychi,target=/shareb/ychi --mount type=bind,src=/sharec/ychi/,target=/sharec/ychi zhuakexi/bioenvs:v0.2.6 /bin/bash \
